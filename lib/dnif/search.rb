0d060662140d42791a7a7df8cbc9ca4baaffd80e
Rafael Souza <rafael.ssouza@gmail.com>
2010-07-15 18:23:10 -0300

    added :classes to .search and using the class index to filter results

--- lib/dnif/search.rb
+++ lib/dnif/search.rb
@@ -3,20 +3,25 @@ module Dnif
   def self.search(query, options = {})
     options.reverse_merge!(:index => '*')
 
-    if options[:class]
-      filter_value = Dnif::MultiAttribute.encode(options[:class]).split(",").map(&:to_i)
-      client.filters = [Riddle::Client::Filter.new("class_id", filter_value)]
+    if options[:classes]
+      ids = if options[:classes].is_a?(Array)
+        options[:classes].map { |name| ActiveRecord::Base.indexes.keys.index(name) }
+      else
+        [ActiveRecord::Base.indexes.keys.index(options[:classes])]
+      end
+
+      client.filters = [Riddle::Client::Filter.new("class_id", ids)]
     end
 
     results = client.query(query, options[:index])
     raise results[:error] if results[:error]
 
     models = results[:matches].inject({}) do |memo, match|
-      class_id = match[:attributes]["class_id"].split(',').flatten
-      class_name = Dnif::MultiAttribute.decode(class_id)
+      encoded_class_name = match[:attributes]["class_name"].split(',').flatten
+      class_name = Dnif::MultiAttribute.decode(encoded_class_name)
 
       memo[class_name] ||= []
-      memo[class_name] << (match[:doc] - class_id.sum { |c| c.to_i })
+      memo[class_name] << (match[:doc] - encoded_class_name.sum { |c| c.to_i })
       memo
     end
 
@@ -45,9 +50,9 @@ module Dnif
   module Search
 
     def search(query)
-      Dnif.search(query, :class => self.name)
+      Dnif.search(query, :classes => self.name)
     end
   end
 end
 
-ActiveRecord::Base.extend(Dnif::Search)
+ActiveRecord::Base.extend(Dnif::Search) if defined?(ActiveRecord::Base)
--- test/unit/test_search.rb
+++ test/unit/test_search.rb
@@ -60,36 +60,57 @@ class TestSearch < Test::Unit::TestCase
   end
 
   test ".search through models" do
-    results_for_post = {
-      :matches => [{
-        :doc => 2983,
-        :attributes => {
-          "class_id" => "336,623,883,1140"
-        }
-      }]
-    }
-    results_for_comment = {
-      :matches => [{
-        :doc => 7893,
-        :attributes => {
-          "class_id" => "323,623,877,1133,1381,1646,1908"
-        }
-      }]
-    }
-
     riddle = mock("Riddle")
     riddle.expects(:query).with("post", "*").returns(results_for_post)
     riddle.expects(:query).with("comment", "*").returns(results_for_comment)
-    riddle.expects(:filters).twice.returns([])
+    riddle.expects(:filters=).twice.returns([])
     Dnif.expects(:client).times(4).returns(riddle)
 
-    Riddle::Client::Filter.expects(:new).with("class_id", [336, 623, 883, 1140])
-    Riddle::Client::Filter.expects(:new).with("class_id", [323, 623, 877, 1133, 1381, 1646, 1908])
+    ActiveRecord::Base.expects(:indexes).twice.returns({ "Post" => mock, "Comment" => mock })
+
+    Riddle::Client::Filter.expects(:new).with("class_id", [0])
+    Riddle::Client::Filter.expects(:new).with("class_id", [1])
 
     Post.expects(:find_all_by_id).once.with([1])
     Comment.expects(:find_all_by_id).once.with([2])
 
     Post.search("post")
     Comment.search("comment")
+  end
+
+  test "search only in specified models" do
+    riddle = mock("Riddle")
+    riddle.expects(:query).with("post", "*").returns(results_for_post)
+    riddle.expects(:filters=).returns([])
+    Dnif.expects(:client).times(2).returns(riddle)
+
+    ActiveRecord::Base.expects(:indexes).returns({ "Post" => mock })
+
+    Riddle::Client::Filter.expects(:new).with("class_id", [0])
+    Post.expects(:find_all_by_id).once.with([1])
+
+    Dnif.search("post", :classes => "Post")
+  end
+
+  def results_for_post
+    {
+      :matches => [{
+        :doc => 2983,
+        :attributes => {
+          "class_name" => "336,623,883,1140"
+        }
+      }]
+    }
+  end
+
+  def results_for_comment
+    {
+      :matches => [{
+        :doc => 7893,
+        :attributes => {
+          "class_name" => "323,623,877,1133,1381,1646,1908"
+        }
+      }]
+    }
   end
 end
