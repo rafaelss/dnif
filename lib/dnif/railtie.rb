8fa8226262f993021ac0403941781f1a73646238
rafaelss <me@rafaelss.com>
2010-09-08 12:37:09 -0300

    improving integration with rails

---
+++ lib/dnif/railtie.rb
@@ -0,0 +1,17 @@
+require "dnif"
+require "rails"
+
+module Dnif
+  class Railtie < Rails::Railtie
+
+    config.to_prepare do
+      Dnif.root_path = Rails.root
+      Dnif.environment = Rails.env
+      Dnif.models_path = File.join(Rails.root, "app", "models")
+    end
+
+    rake_tasks do
+      load "dnif/tasks"
+    end
+  end
+end
