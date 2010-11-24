ab9d1470c05c311745ac17facd9f2596a1c31db1
rafaelss <me@rafaelss.com>
2010-11-24 12:55:27 -0200

    bump up version

--- lib/dnif/version.rb
+++ lib/dnif/version.rb
@@ -2,8 +2,8 @@ module Dnif
   module Version
     MAJOR = 0
     MINOR = 1
-    PATCH = 0
-    BUILD = nil # "beta.6"
+    PATCH = 1
+    BUILD = nil
 
     STRING = [MAJOR, MINOR, PATCH, BUILD].compact.join('.')
   end
