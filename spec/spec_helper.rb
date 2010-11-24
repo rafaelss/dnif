3fd256c03e768805cf8cb0471a993d14fb89adf9
rafaelss <me@rafaelss.com>
2010-11-24 12:55:15 -0200

    without jeweler, I need to require gems manually, I dont know why either

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,8 @@
 # encoding: utf-8
-require "bundler"
-Bundler.require(:default, :development)
+require "bundler/setup"
+require "active_record"
+require "rspec"
+require "database_cleaner"
 
 $:.unshift(File.dirname(__FILE__) + '/../lib')
 require "dnif"
